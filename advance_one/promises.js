83ecd2fc532eec03e15a3ec56734495f01a9e151
runningpoem30 <aryaanandpathak30@gmail.com>
2024-06-18 21:33:06 +0530

    Update promises.js

--- advance_one/promises.js
+++ advance_one/promises.js
@@ -31,29 +31,68 @@
 //   console.log(user);
 // });
 
-const promiseFour = new Promise(function (resolve, reject) {
+// const promiseFour = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = false;
+//     if (!error) {
+//       resolve({ username: "Arya", password: "123" });
+//     } else {
+//       reject("Error : something went wrong");
+//     }
+//   }, 2000);
+// });
+
+// promiseFour
+//   .then((user) => {
+//     console.log(user);
+//     return user.username;
+//   })
+//   .then((username) => {
+//     console.log(username);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(() => console.log("the promise is either resolved or rejected"));
+
+const promiseFive = new Promise(function (resolve, reject) {
   setTimeout(function () {
     let error = false;
     if (!error) {
-      resolve({ username: "Arya", password: "123" });
+      resolve({ username: "Javascript", password: "123" });
     } else {
-      reject("Error : something went wrong");
+      reject("Error : JS  went wrong");
     }
   }, 2000);
 });
 
-promiseFour
-  .then((user) => {
-    console.log(user);
-    return user.username;
-  })
-  .then((username) => {
-    console.log(username);
-  })
-  .catch(function (error) {
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
     console.log(error);
+  }
+}
+consumePromiseFive();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error("Error fetching users:", error);
+//   }
+// }
+
+// getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
   })
-  .finally(() => console.log("the promise is either resolved or rejected"));
-
-
-  const promiseFive 
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
